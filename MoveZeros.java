2067eab62d43d3a84c78579c846d81b111612481
ngbcoder94 <ngbcoder94@gmail.com>
2025-01-25 22:42:24 -0500

    First loop complete

--- MoveZeros.java
+++ MoveZeros.java
@@ -16,6 +16,19 @@ class MoveZeros{
          */
         public void zeros(int[] nums){
 
+                int insertPos = 0;  //Need to keep track of where to place the next non-zero element.
+
+                //Now I need to move the non-zero elements. 
+                for(int i = 0; i < nums.length; i++){
+                        
+                        //Check if I have a valid element. 
+                        if(nums[i] != 0){
+                                nums[insertPos] = nums[i];  //Update the array accordingly if not a 0.
+                                insertPost ++;              //Increment the insertPos variable.
+                        }
+                }
+
+
         }
 
         //Here is my public method for testing/running MoveZeros.
