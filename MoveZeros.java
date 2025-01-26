e93ecf4ac42aa7ffd930bb9c84442a0d6b223a40
ngbcoder94 <ngbcoder94@gmail.com>
2025-01-25 22:34:24 -0500

    Class created to solve solution

---
+++ MoveZeros.java
@@ -0,0 +1,14 @@
+/*
+ * This class will be used to solve the Leetcode problem MoveZeros. 
+ *
+ * Author: Nick Burkett
+ * Date: 01/25/25
+ *
+ * Status: WIP
+ */
+class MoveZeros{
+        public static void main(String[] args){
+
+                System.out.println("Hello World");
+        }
+}
