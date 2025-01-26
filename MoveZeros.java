4aaafee62ba942c285dd79f2838927e00e0a9c48
ngbcoder94 <ngbcoder94@gmail.com>
2025-01-25 22:37:22 -0500

    Making progress

--- MoveZeros.java
+++ MoveZeros.java
@@ -7,6 +7,18 @@
  * Status: WIP
  */
 class MoveZeros{
+
+        /*
+         * Here is my zeros method that will be used to solve the problem at hand.
+         *
+         * @params int[] nums - Array passed in to he modified.
+         * @return void - Don't return anything.
+         */
+        public void zeros(int[] nums){
+
+        }
+
+        //Here is my public method for testing/running MoveZeros.
         public static void main(String[] args){
 
                 System.out.println("Hello World");
